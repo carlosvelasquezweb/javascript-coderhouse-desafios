b73ab4671c2db4fc13cf18a5f4e9158694104bdc
Carlos Velasquez <carlosvelasquezweb@gmail.com>
2021-12-16 01:17:31 -0300

    Subida al servidor

---
+++ segundo-desafio.js
@@ -0,0 +1,26 @@
+// #1 - Primer Condicional verifica si el nombre es igual al que está registrado en la base de datos.
+
+/* let nombre = prompt("Por favor ingrese su nombre").toLowerCase();
+const dataBase = "jean";
+
+if (nombre == dataBase) {
+  swal(`Hola ${dataBase}, que bueno de verte de nuevo!`);
+} else {
+  swal(`Lo sentimos ${nombre}, no hemos encontrado tu nombre en la base de datos :(`);
+}
+ */
+
+// #2 Verifica si es mayor o menor de edad, en caso de ser menor de edad le indica la cantidad de años que le faltan para conducir
+
+let edad = Number(prompt("¿Cuantos años tienes?"));
+let tiempo = 17 - edad;
+
+if (edad >= 17) {
+  swal("Excelente ya tienes edad para conducir!");
+} else if (tiempo === 1) {
+  swal(`Lo sentimos te falta ${tiempo} año para conducir!`);
+ } else if (edad === 0) {
+    swal(`Lo sentimos, usted aun no ha nacido! `);
+}  else {
+  swal(`Lo sentimos te faltan ${tiempo} años para conducir!`);
+}
